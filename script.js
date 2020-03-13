f5c52c4904ffeffb97f30a0b63bd45a5579a915d
Eduard.Brukish <eduardbrukish@gmail.com>
2020-03-13 13:58:43 +0200

    feat: add activating functionality to portfolio section

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const navigation = document.querySelector('.navigation');
+const portfolioMenu = document.querySelector('.work-examples__menu');
+const portfolioExamples = document.querySelector('.work-examples__images');
+
 navigation.addEventListener('click', activateMenuItem);
+portfolioMenu.addEventListener('click', sortPortfolioExamples);
 
 function activateMenuItem(event) {
     event.preventDefault();
@@ -14,3 +18,15 @@ function activateMenuItem(event) {
         block: 'start'
     });
 };
+
+function sortPortfolioExamples(event) {
+    if (event.target === null || event.target.classList.contains('work-examples__menu-item_active')) return;
+
+    portfolioMenu.querySelectorAll('.work-examples__menu-item').forEach(element => element.classList.remove('work-examples__menu-item_active'));
+    event.target.classList.add('work-examples__menu-item_active');
+    portfolioExamples.querySelectorAll('.work-examples__images-item')
+        .forEach(item => {
+            item.style.order = Math.round(Math.random() * 12);
+        }
+        );
+}
